290541eac93dd92df0f3ec703047a5262e875be6
aluy <79387054+aluy-ab@users.noreply.github.com>
2023-03-29 14:32:48 -0300

    javascript em um arquivo separado

---
+++ aula6/js_separado.js
@@ -0,0 +1,9 @@
+// let nome = "Ana";
+// let idade ="137";
+// alert(nome + " tem " + idade + " anos ");
+let nome = window.prompt("Digite seu nome:");
+alert("Olá, " + nome);
+let num1 = window.prompt("Digite um número:");
+let num2 = window.prompt("Digite outro número:");
+let soma = parseInt(num1) + parseInt(num2);
+alert("Soma: " + soma);
