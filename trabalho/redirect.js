82c464c31fe243eaeaf0653ba5de03780d163d3a
aluy <79387054+aluy-ab@users.noreply.github.com>
2023-05-16 22:31:35 -0300

    Add files via upload

---
+++ trabalho/redirect.js
@@ -0,0 +1,11 @@
+function redirect_login() {
+    location.href = "loginpage.html";
+}
+
+function redirect_main() {
+    location.href = "mainpage.html";
+}
+
+function redirect_cadastro() {
+    location.href = "cadastropage.html";
+}
